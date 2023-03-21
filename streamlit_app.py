ff567ad67a7816737d94aed44c41a39aa8e1b888
JESSF121 <128154967+JESSF121@users.noreply.github.com>
2023-03-21 16:18:02 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,13 +45,6 @@ except URLError as e:
 
 
 
-
-
-
-
-
-
-
 import snowflake.connector
 
 streamlit.header("The fruit load list contains:")
@@ -70,13 +63,3 @@ if streamlit.button('Get Fruit Load List'):
 
 
 
-
-
-
-
-# Allow the end user to add a fruit to the list
-add_my_fruit = streamlit.text_input ('What fruit would you like to add?' , 'jackfruit')
-streamlit.write ('Thanks for adding' , add_my_fruit)
-
-#This will not work correctly, but just go with it for now
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
